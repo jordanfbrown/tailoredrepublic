a1b053941ebfc000beff26ac2e1d2c5c767668a6
Jordan Brown <jordanfbrown@gmail.com>
2012-12-01 20:21:22 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TailoredRepublic::Application.config.secret_token = '08b21068133282407329ebe8e3c908d791f2a74094302fc2ee67f98eba9a6584b79b5f5c6088f644c8ee623a5b3c1e8020c27e3732f52cc79fac512c042ab474'
