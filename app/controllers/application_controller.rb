2956b28aaf60db887080c0bc9be10009745f4cae
Jordan Brown <jordanfbrown@gmail.com>
2012-12-04 00:36:48 -0800

    Adding basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  protected
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "tailored" && password == "sodamnclean!"
+      end
+    end
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
