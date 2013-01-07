a3bd12496c72418a5c3fd63ae7b34afdf1a54d14
Jordan Brown <jordanfbrown@gmail.com>
2013-01-07 14:01:13 -0800

    Customizations should be deleted when a CartItem is deleted

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ActiveRecord::Base
   attr_accessible :cart_id, :product, :customization
   belongs_to :cart
   belongs_to :product
-  belongs_to :customization
+  belongs_to :customization, dependent: :destroy
 
   def total_price
     (product.price + (customization.vest? ? Product.vest_price : 0)).to_i
