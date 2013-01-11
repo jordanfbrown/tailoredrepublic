f84bea186c711761f4b83735b50e99e1431b28aa
Jordan Brown <jordanfbrown@gmail.com>
2013-01-11 14:37:29 -0800

    Adding name to address table

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
-  attr_accessible :city, :line1, :line2, :state, :zip, :type
+  attr_accessible :name, :city, :line1, :line2, :state, :zip, :type
 
   validates :zip, format: { with: /^\d{5}(-\d{4})?$/, message: 'Zip should be in the form 12345 or 12345-1234' }
 end
