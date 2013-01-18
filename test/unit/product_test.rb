1b50c57ccc1b37d0c477581d147f0242bd5c2fe5
Jordan Brown <jordanfbrown@gmail.com>
2013-01-17 21:19:10 -0800

    Adding tests for product model

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,4 +1,24 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  test "depluralize_category" do
+    assert_equal Product.depluralize_category('suits'), :suit
+    assert_equal Product.depluralize_category('shirts'), :shirt
+    assert_equal Product.depluralize_category('pants'), :pants
+    assert_equal Product.depluralize_category('accessories'), :accessories
+    assert_equal Product.depluralize_category('foo'), :suit
+  end
+
+  test "can't delete a product if its referenced by a line item" do
+    product = products(:charcoal)
+    LineItem.new(product: product)
+    product.destroy
+    assert product.errors.any?
+  end
+
+  test "can' delete a product if its not referenced by a line item" do
+    product = Product.new
+    product.destroy
+    assert !product.errors.any?
+  end
 end
