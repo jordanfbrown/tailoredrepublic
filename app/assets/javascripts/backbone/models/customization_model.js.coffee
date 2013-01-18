82080d77b54026368141c307d604c96777cea085
Jordan Brown <jordanfbrown@gmail.com>
2013-01-18 11:42:06 -0800

    Adding ability to edit customizations for shirts

--- app/assets/javascripts/backbone/models/customization_model.js.coffee
+++ app/assets/javascripts/backbone/models/customization_model.js.coffee
@@ -1,30 +1,33 @@
 class TR.Models.Customization extends TR.Models.Base
   initialize: (attributes, options) ->
-    @productCategory = options.category
+    @productCategory = options.category if options
 
-    if @productCategory == 'suit'
-      @attributes = _.extend {}, {
-        lapel: 'notch'
-        buttons: 2
-        vents: 1
-        pleats: 0
-        pant_cuffs: false
-        fit: 'tailored'
-        lining: 0
-        monogram: ''
-        bouttoniere: true
-        pick_stitching: false
-        vest: false
-        vest_buttons: 3
-      }, attributes
-    else if @productCategory == 'shirt'
-      @attributes = _.extend {}, {
-        collar: 'standard'
-        fit: 'slim'
-        pocket: false
-        monogram: ''
-        monogram_color: 'white'
-      }, attributes
+    if @productCategory
+      if @productCategory == 'suit'
+        @attributes = _.extend {}, {
+          lapel: 'notch'
+          buttons: 2
+          vents: 1
+          pleats: 0
+          pant_cuffs: false
+          fit: 'tailored'
+          lining: 0
+          monogram: ''
+          bouttoniere: true
+          pick_stitching: false
+          vest: false
+          vest_buttons: 3
+          product_category: @productCategory
+        }, attributes
+      else if @productCategory == 'shirt'
+        @attributes = _.extend {}, {
+          collar: 'standard'
+          fit: 'slim'
+          pocket: false
+          monogram: ''
+          monogram_color: 'white'
+          product_category: @productCategory
+        }, attributes
 
 
   url: ->
--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,19 +1,28 @@
 module CartsHelper
   def customization_list(customization)
-    [
-      { option: "Lapel", type: customization.lapel.capitalize },
-      { option: "Buttons", type: wordify(customization.buttons) },
-      { option: "Vents", type: wordify(customization.vents) },
-      { option: "Pleats", type: wordify(customization.pleats) },
-      { option: "Pant Cuffs", type: wordify(customization.pant_cuffs) },
-      { option: "Fit", type: customization.fit.capitalize },
-      { option: "Lining", type: customization.lining },
-      { option: "Monogram", type: customization.monogram.length == 0 ? "None" : customization.monogram },
-      { option: "Bouttoniere", type: wordify(customization.bouttoniere) },
-      { option: "Pick Stitching", type: wordify(customization.pick_stitching) },
-      { option: "Vest", type:
-        (customization.vest ? "#{customization.vest_buttons} Button Vest (+$#{Product.vest_price})" : "No") }
-    ]
+    if customization.product_category == 'suit'
+      [
+        { option: "Lapel", type: customization.lapel.capitalize },
+        { option: "Buttons", type: wordify(customization.buttons) },
+        { option: "Vents", type: wordify(customization.vents) },
+        { option: "Pleats", type: wordify(customization.pleats) },
+        { option: "Pant Cuffs", type: wordify(customization.pant_cuffs) },
+        { option: "Fit", type: customization.fit.capitalize },
+        { option: "Lining", type: customization.lining },
+        { option: "Monogram", type: customization.monogram.length == 0 ? "None" : customization.monogram },
+        { option: "Bouttoniere", type: wordify(customization.bouttoniere) },
+        { option: "Pick Stitching", type: wordify(customization.pick_stitching) },
+        { option: "Vest", type:
+          (customization.vest ? "#{customization.vest_buttons} Button Vest (+$#{Product.vest_price})" : "No") }
+      ]
+    elsif customization.product_category == 'shirt'
+      [
+        { option: "Collar", type: customization.collar.capitalize },
+        { option: "Fit", type: customization.fit.capitalize },
+        { option: "Pocket", type: wordify(customization.pant_cuffs) },
+        { option: "Monogram", type: customization.monogram.length == 0 ? "None" : customization.monogram },
+      ]
+    end
   end
 
   def customization_to_key(customization)
