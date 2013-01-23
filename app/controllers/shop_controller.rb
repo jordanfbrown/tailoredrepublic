5e8ec11c986bcf841632fa7331a51cddee56826d
Jordan Brown <jordanfbrown@gmail.com>
2013-01-23 14:27:39 -0800

    Adding header to shop view

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -2,5 +2,6 @@ class ShopController < ApplicationController
   def index
     category = Product.depluralize_category params[:category]
     @products = Product.where(category: category)
+    @display_category = params[:category].capitalize
   end
 end
