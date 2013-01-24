da779684862b4294a32b4776babb1a55b19ed565
Jordan Brown <jordanfbrown@gmail.com>
2013-01-24 15:56:36 -0800

    Fixing coming soon page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,14 +19,16 @@ class PagesController < ApplicationController
 
   def request_access_code
     PotentialUser.create(email: params[:email])
-    redirect_to root_path, notice: true
+    redirect_to root_path, notice: :new_user
   end
 
   def authenticate_access_code
     if params[:access_code] == ENV['ACCESS_CODE']
       cookies[:access_code] = params[:access_code]
+      redirect_to root_path
+    else
+      redirect_to root_path, notice: :error
     end
 
-    redirect_to root_path
   end
 end
