a96a79a37887d90c2bb883b9c14e855357380316
Jordan Brown <jordanfbrown@gmail.com>
2013-01-30 11:38:06 -0800

    Only validate phone number in special validation method

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,12 +4,14 @@ class Address < ActiveRecord::Base
 
   validates_presence_of :name, :city, :line1, :state, :zip
   validates :zip, format: { with: /^\d{5}(-\d{4})?$/, message: 'should be in the form 12345 or 12345-1234' }
-  validates :phone_number, format: { with: /^[\(\)0-9\- \+\.]{10,20}$/, message: 'must be a valid telephone number' }
 
   def valid_with_phone?
     valid?
     if phone_number.blank?
-      errors.add(:phone, 'is required')
+      errors.add(:phone_number, 'is required')
+    end
+    unless phone_number.match /^[\(\)0-9\- \+\.]{10,20}$/
+      errors.add(:phone_number, 'is a not a valid phone number')
     end
     errors.count == 0
   end
