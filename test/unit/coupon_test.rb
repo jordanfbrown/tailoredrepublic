80c6fdbb6ebb8df7381b1e28f09d44ccb648d165
Jordan Brown <jordanfbrown@gmail.com>
2013-01-31 09:49:00 -0800

    More test cases for coupons

--- test/unit/coupon_test.rb
+++ test/unit/coupon_test.rb
@@ -28,7 +28,7 @@ class CouponTest < ActiveSupport::TestCase
     assert_equal coupon.calculate_discount(1000), 100
   end
 
-  test "apply_to_order" do
+  test "apply_to_order where total comes out to 0" do
     coupon = coupons(:hundred_gift_card)
     order = orders(:gift_card_order) # $100 order
     coupon_quantity_before = coupon.quantity
@@ -38,6 +38,17 @@ class CouponTest < ActiveSupport::TestCase
     assert_equal order.final_cost, 0
     assert_equal coupon.quantity, coupon_quantity_before - 1
     assert_equal coupon.amount, 0
+  end
+
+  test "apply_to_order where gift card amount is more than order ($400 gift card, $100 order)" do
+    coupon = coupons(:four_hundred_gift_card)
+    order = orders(:shirt_order)
+    coupon.apply_to_order(order)
+
+    assert_equal order.discount, 100
+    assert_equal order.final_cost, 0
+    assert_equal coupon.quantity, 1  # Coupon still exists
+    assert_equal coupon.amount, 300 # Amount of order has been subtracted
   end
 
   test "create_coupons_from_order" do
