1601e2cba73ae059baee42509ddaf5dba6457b37
Jordan Brown <jordanfbrown@gmail.com>
2013-01-31 09:42:03 -0800

    Setting up better cart/line item fixtures

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -2,29 +2,37 @@ require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
   test "total_price" do
-    cart = carts(:empty)
-    product = products(:executive)
-    product.price = 500
-    product.save
-    LineItem.create(product: product, cart_id: cart.id, customization: customizations(:one))
-    LineItem.create(product: product, cart_id: cart.id, customization: customizations(:two))
-    assert_equal cart.total_price, 1000
+    cart = carts(:one_shirt_one_suit)
+    assert_equal cart.total_price, 629
   end
 
   test "shirt_only? should be false if there is a suit in the cart and no shirts" do
-    cart = carts(:one)
+    cart = carts(:one_suit)
     assert !cart.shirt_only?
   end
 
   test "shirt_only? should be false if there is at least 1 shirt and at least 1 suit in the cart" do
-    cart = carts(:one)
-    cart.line_items << LineItem.new(product: products(:white_shirt), customization: customizations(:one))
+    cart = carts(:one_shirt_one_suit)
     assert !cart.shirt_only?
   end
 
   test "shirt_only? should be true if there is at least 1 shirt and no suit in the cart" do
-    cart = carts(:empty)
-    cart.line_items << LineItem.new(product: products(:white_shirt), customization: customizations(:one))
+    cart = carts(:one_shirt)
     assert cart.shirt_only?
+  end
+
+  test "skip_measurements? should be false if there is a suit in the cart" do
+    cart = carts(:one_suit)
+    assert !cart.skip_measurements?
+  end
+
+  test "skip_measurements? should be false if there is a shirt in the cart" do
+    cart = carts(:one_shirt)
+    assert !cart.skip_measurements?
+  end
+
+  test "skip_measurements? should be true if there is a no shirt or suit in the cart" do
+    cart = carts(:one_shirt)
+    assert !cart.skip_measurements?
   end
 end
