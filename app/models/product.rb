4d3bba4497d5cc2831d4c2a62e079844ae1418b8
Jordan Brown <jordanfbrown@gmail.com>
2013-02-13 15:53:27 -0800

    Adding summary field to products index and edit form

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ require 'enumerated_attribute'
 class Product < ActiveRecord::Base
   enum_attr :category, %w(suit shirt accessory gift_card)
   attr_accessible :description, :image_large_url, :image_small_1_url, :image_small_2_url, :image_small_3_url,
-                  :image_small_4_url, :name, :price, :quantity, :category
+                  :image_small_4_url, :name, :price, :quantity, :category, :summary
   has_many :line_items
   has_many :customizations
   before_destroy :ensure_not_referenced_by_line_item
