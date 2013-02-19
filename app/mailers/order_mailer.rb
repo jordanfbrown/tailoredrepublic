9dcb47aedb2615d40683ae4c83219a47e04a2936
Jordan Brown <jordanfbrown@gmail.com>
2013-02-19 15:55:38 -0800

    Adding text version of order confirmation email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,11 +6,18 @@ class OrderMailer < ActionMailer::Base
   def order_confirmation_email(order)
     @order = order
     @user = order.user
-    mail(to: @user.email, bcc: 'orders@tailoredrepublic.com', subject: "Tailored Republic Order Confirmation (#{order.order_id})")
+    mail(
+      to: @user.email,
+      bcc: 'orders@tailoredrepublic.com',
+      subject: "Tailored Republic Order Confirmation (#{order.order_id})",
+    )
   end
 
   def order_fulfillment_email(order)
     @order = order
-    mail(to: 'jordan@tailoredrepublic.com, jeff@tailoredrepublic.com, brian@tailoredrepublic.com', subject: "New Order (#{order.order_id})")
+    mail(
+      to: 'jordan@tailoredrepublic.com, jeff@tailoredrepublic.com, brian@tailoredrepublic.com',
+      subject: "New Order (#{order.order_id})"
+    )
   end
 end
