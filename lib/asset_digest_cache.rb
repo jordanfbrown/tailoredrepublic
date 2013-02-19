7b3faa6384a325f63f77695788def429de225f44
Jordan Brown <jordanfbrown@gmail.com>
2013-02-18 19:37:41 -0800

    Only include png and jpg files in asset digest json

--- lib/asset_digest_cache.rb
+++ lib/asset_digest_cache.rb
@@ -2,7 +2,11 @@ class AssetDigestCache
   @@asset_cache = {}
   def self.get_digest_hash
     if @@asset_cache.length == 0
-      Rails.application.assets.each_logical_path { |a| @@asset_cache[a] = Rails.application.assets.find_asset(a).digest_path }
+      Rails.application.assets.each_logical_path do |a|
+        if a.include?('png') || a.include?('jpg')
+          @@asset_cache[a] = Rails.application.assets.find_asset(a).digest_path
+        end
+      end
     end
     @@asset_cache
   end
