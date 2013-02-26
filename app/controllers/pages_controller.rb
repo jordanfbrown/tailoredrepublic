c764a9a7da84fc9a83aeb06045d950df14f2be68
Jordan Brown <jordanfbrown@gmail.com>
2013-02-26 15:43:54 -0800

    Ignore mailchimp error on schedule tailor signup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,11 @@ class PagesController < ApplicationController
     if @scheduling_info.valid?
       if info[:subscribe_to_mailing_list] == "1"
         gibbon = Gibbon.new
-        gibbon.list_subscribe(id: GIBBON_LIST_ID, email_address: info[:email])
+        begin
+          gibbon.list_subscribe(id: GIBBON_LIST_ID, email_address: info[:email])
+        rescue
+          puts 'Ignoring error'
+        end
       end
       ScheduleTailoringMailer.schedule_tailoring_email(info[:name], info[:email], info[:zip_code], info[:phone_number]).deliver
       redirect_to '/shop/suits', notice: 'We have received your request to schedule a tailoring. A Tailored Republic representative will contact you with details in the next 24 hours.'
