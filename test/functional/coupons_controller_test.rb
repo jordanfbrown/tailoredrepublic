69f0f8fc689f0a7ca0674b70b811f52b148cd155
Jordan Brown <jordanfbrown@gmail.com>
2013-03-04 19:47:18 -0800

    Adding test for coupons controller

---
+++ test/functional/coupons_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class CouponsControllerTest < ActionController::TestCase
+  setup do
+    sign_in :user, users(:admin)
+    @coupon_params = {
+      code: 'ABCDEFGH',
+      description: 'Test',
+      quantity: 1,
+      amount: 200,
+      coupon_type: 'promotion',
+      discount_type: 'fixed'
+    }
+  end
+
+  test 'should be able to get index' do
+    get :index
+    assert :success
+    assert_template :index
+  end
+
+  test 'should be able to get new' do
+    get :new
+    assert :success
+    assert_template :new
+  end
+
+  test 'should be able to edit' do
+    coupon = coupons(:hundred_gift_card)
+    get :edit, id: coupon.id
+    assert :success
+    assert_template :edit
+    assert_equal assigns(:coupon), coupon
+  end
+
+  test 'should be able to create a coupon' do
+    count = Coupon.count
+    post :create, coupon: @coupon_params
+    assert_equal Coupon.count, count + 1
+    assert :success
+  end
+
+  test 'should be able to update a coupon' do
+    coupon = coupons(:hundred_gift_card)
+    put :update, id: coupon.id, coupon: @coupon_params
+    assert :success
+    coupon.reload
+    assert_equal coupon.code, @coupon_params[:code]
+  end
+
+  test 'should be able to delete a coupon' do
+    coupon = coupons(:hundred_gift_card)
+    delete :destroy, id: coupon.id
+    assert :success
+    assert_raise(ActiveRecord::RecordNotFound) { coupon.reload }
+  end
+
+end
