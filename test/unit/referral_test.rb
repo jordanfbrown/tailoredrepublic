3bf2acee6ba8a76ad817f52fb705149ffcfe2578
Jordan Brown <jordanfbrown@gmail.com>
2013-03-15 10:49:17 -0700

    Adding tests for referral model

--- test/unit/referral_test.rb
+++ test/unit/referral_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ReferralTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "order_completed should add $20 to the referrer's credit, and update status to STATUS_COMPLETED" do
+    referral = referrals(:referral_created)
+    referrer = referral.referrer
+    referrer_credit = referrer.referral_credit
+
+    referral.order_completed
+    referral.reload
+    referrer.reload
+
+    assert_equal referral.status, Referral::STATUS_COMPLETED
+    assert_equal referrer_credit + 20, referrer.referral_credit
+  end
+
+  test "order_completed should not credit the referrer if the current referral status is STATUS_COMPLETED" do
+    referral = referrals(:referral_completed)
+    referrer = referral.referrer
+    referrer_credit = referrer.referral_credit
+
+    referral.order_completed
+    referrer.reload
+
+    assert_equal referrer_credit, referrer.referral_credit
+  end
 end
